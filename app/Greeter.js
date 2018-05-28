474a07b90f9624361d2faffd495ae909e20c7d2c
volleAB <724958674@qq.com>
2018-05-28 22:26:27 +0800

    error

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,5 +1,26 @@
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
+var config = require('./config.json');
+
+/**module.exports = function() {
+    let greet = document.createElement('div');
+    greet.classList.add('greet');
+    let blue = document.createElement('div');
+    blue.classList.add('blue');
+    greet.textContent = config.greetText;
+    blue.textContent = config.blueText;
+    return greet,blue;
 };
+*/
+
+class Greet {
+    greet () {
+        let greet = document.createElement('div');
+        greet.classList.add('greet');
+        let blue = document.createElement('div');
+        blue.classList.add('blue');
+        greet.textContent = config.greetText;
+        blue.textContent = config.blueText;
+    }
+}
+
+
+module.exports = Greet;
